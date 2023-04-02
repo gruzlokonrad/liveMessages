bc6ab768057a52a511f0987e1b5fb74e90991c70
Konrad Gruzlo <gruzlokonrad@gmail.com>
2023-04-02 20:11:45 +0200

    add disconnect event and logged event

--- client/public/app.js
+++ client/public/app.js
@@ -4,6 +4,9 @@ const messagesList = document.querySelector('#messages-list');
 const addMessageForm = document.querySelector('#add-messages-form');
 const userNameInput = document.querySelector('#username');
 const messageContentInput = document.querySelector('#message-content');
+const socket = io({
+  autoConnect: false
+});
 
 let userName;
 
@@ -13,36 +16,42 @@ const login = (event) => {
     alert('Fill in your name, please.')
   } else {
     userName = userNameInput.value
+    socket.emit('logged', userName)
     loginForm.classList.remove('show')
     messagesSection.classList.add('show')
+    socket.open()
   }
 }
 
 const sendMessage = (event) => {
-  const addMessage = (name, content) => {
-    const author = name
-    const message = document.createElement('li')
-    message.classList.add('message')
-    message.classList.add('message--received')
-
-    if (author === userName) message.classList.add('message--self')
-    message.innerHTML = `
-    <h3 class="message__author">${userName === author ? 'You' : author}</h3>
-    <div class="message__content">
-      ${content}
-    </div>
-    `
-    messagesList.appendChild(message)
-  }
-
   event.preventDefault()
-  if (!messageContentInput?.value) {
+  let messageContent = messageContentInput.value
+  if (!messageContent.length) {
     alert('Your message is empty')
   } else {
-    addMessage(userName, messageContentInput.value)
+    addMessage(userName, messageContent)
+    socket.emit('message', { author: userName, content: messageContent })
     messageContentInput.value = ''
   }
 }
 
+const addMessage = (name, content) => {
+  const author = name
+  const message = document.createElement('li')
+  message.classList.add('message')
+  message.classList.add('message--received')
+
+  if (author === 'Chat Bot') message.classList.add('message--bot')
+  if (author === userName) message.classList.add('message--self')
+  message.innerHTML = `
+  <h3 class="message__author">${userName === author ? 'You' : author}</h3>
+  <div class="message__content">
+    ${content}
+  </div>
+  `
+  messagesList.appendChild(message)
+}
+
 loginForm.addEventListener('submit', (event) => login(event))
 addMessageForm.addEventListener('submit', (event) => sendMessage(event))
+socket.on('message', ({ author, content }) => addMessage(author, content))
--- server.js
+++ server.js
@@ -2,29 +2,69 @@ import express from 'express'
 import cors from 'cors'
 import path, { dirname } from 'path'
 import { fileURLToPath } from 'url';
+import { Server } from 'socket.io'
+import { createServer } from "http";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
-
-// Serve static files from the React app
-// app.use(express.static(path.join(__dirname, '/client/build')));
 app.use(express.static(path.join(__dirname, '/client/public')))
-// app.use('/api', testimonialsRoute)
 
 const message = []
+let users = []
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/client/index.html'));
 });
 
-// app.use((req, res) => {
-//   res.status(404).send({ message: "Not found ..." })
-// })
+const server = createServer(app);
+const io = new Server(server, { allowEIO3: true });
+io.on('connection', (socket) => {
+  console.log('New client! Its id – ' + socket.id);
+  console.log('I\'ve added a listener on message event \n');
 
-app.listen(process.env.PORT || 8000, () => {
+  socket.on('message', (event) => {
+    console.log('Oh, I\'ve got something from ' + socket.id)
+    // console.log(`${event.author}: ${event.content}`)
+    message.push(event)
+    socket.broadcast.emit('message', event)
+    // io.to('UQy3C7XWRBxJ0RxeAAAE').emit('message', event)
+  });
+
+  socket.on('logged', (user) => {
+    users.push({ user, id: socket.id })
+    socket.broadcast.emit(
+      'message',
+      { author: 'Chat Bot', content: `${user} has joined the conversation!` }
+    )
+    console.log('users', users)
+  })
+
+  socket.on('disconnect', () => {
+    let person;
+    users = users.filter(el => {
+      if (el.id !== socket.id) return el
+      person = el
+    })
+
+    if (person?.user) {
+      console.log('Oh, ' + person?.user + ' has left')
+    } else {
+      console.log('Oh, socket ' + socket.id + ' has left')
+    }
+
+    socket.broadcast.emit(
+      'message',
+      { author: 'Chat Bot', content: `${person?.user} has left the conversation... :(` }
+    )
+  });
+});
+
+
+server.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
 });
